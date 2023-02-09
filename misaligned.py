7b2e08b7ee194fd267225fcd3e84b5ec663f975d
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-09 10:11:58 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,4 +1,4 @@
-def print_color_map():
+def get_color_map():
     COLOR_MAP = dict()
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
@@ -12,7 +12,7 @@ def get_pair_number(color_map, color_pair):
     pair_number = list(filter(lambda idx: color_map[idx] == color_pair, color_map))[0]
     return pair_number
 
-color_map = print_color_map()
+color_map = get_color_map()
 pair_number = get_pair_number(color_map, 'White Blue')
 assert(pair_number == 1)
 print("All is well (maybe!)\n")
