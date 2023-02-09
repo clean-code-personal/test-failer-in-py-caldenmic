9353beb618edb1563da7e6ec5fc5c2ea6e3cabf3
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-09 08:37:25 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -27,6 +27,6 @@ def alert_failure_count_test(failure_count):
 
 failure_count = alert_in_celcius(400.5)
 failure_count = alert_in_celcius(303.6)
-alest_failure_count(failure_count)
+alert_failure_count(failure_count)
 print(f'{alert_failure_count} alerts failed.')
 print('All is well (maybe!)')
