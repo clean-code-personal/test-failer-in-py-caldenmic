5f25a0f3721714f5034375879002b88408a6b596
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-11 13:18:16 +0530

    Create tshirts_test_runner.py

---
+++ tshirts_test_runner.py
@@ -0,0 +1,8 @@
+from tshirts_test_functions import TestTshirts
+
+test_object = TestTshirts()
+
+test_object.test_small_size(37)
+test_object.test_small_size(38)
+test_object.test_medium_size(40)
+test_object.test_large_size(43)
