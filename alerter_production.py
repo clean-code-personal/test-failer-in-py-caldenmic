10c43e8e6955cdf15dfba9dd75ca001b5b3afd85
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-14 00:42:33 +0530

    Update alerter_production.py

--- alerter_production.py
+++ alerter_production.py
@@ -10,14 +10,14 @@ def network_alert_stub(celcius):
     else:
         return 500
 
-def alert_in_celcius(farenheit):
+def alert_in_celcius(farenheit, stub_function):
     celcius = (farenheit - 32) * 5 / 9
-    returnCode = network_alert_stub(celcius)
+    returnCode = stub_function(celcius)
     if returnCode != 200:
         # non-ok response is not an error! Issues happen in life!
         # let us keep a count of failures to report
         # However, this code doesn't count failures!
         # Add a test below to catch this bug. Alter the stub above, if needed.
         global alert_failure_count
-        alert_failure_count += 0
+        alert_failure_count += 1
         return alert_failure_count
